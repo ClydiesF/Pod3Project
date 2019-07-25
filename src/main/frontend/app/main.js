60b5e3dab214505dfc37f2a2145928ad25c2df53
Clyde Freeman <clydefreeman@nikkitas-MacBook-Pro.local>
2019-07-25 15:18:39 -0400

    worked on frontend homepage code

--- src/main/frontend/app/main.js
+++ src/main/frontend/app/main.js
@@ -1,35 +1,52 @@
 import React from "react";
 import ReactDom from "react-dom";
+import HomePageContainer from './HomePageContainer';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      pets: []
-    }
-  }
+//class App extends React.Component {
+//  constructor(props) {
+//    super(props)
+//    this.state = {
+//      pets: []
+//    }
+//  }
+//
+//  componentDidMount() {
+//    fetch("/api/v1/pets").then((resp) => {
+//      if(resp.ok) {
+//        return resp
+//      }
+//      else {
+//        throw new Error(resp.Error)
+//      }
+//    }).then(resp => {
+//      return resp.json();
+//    }).then(petsPayload => {
+//      this.setState({pets: petsPayload.content})
+//    })
+//  }
+//
+//  render() {
+//    const petListItems = this.state.pets.map((pet) => {
+//      return (<li><h2>{ pet.name }</h2><p>{ pet.species}</p></li>)
+//    })
+//    return (<ul>{petListItems}</ul>)
+//  }
+//
+////  return things
+//return(
+//<h1>Hey man</h1>
+//)
+//}
 
-  componentDidMount() {
-    fetch("/api/v1/pets").then((resp) => {
-      if(resp.ok) {
-        return resp
-      }
-      else {
-        throw new Error(resp.Error)
-      }
-    }).then(resp => {
-      return resp.json();
-    }).then(petsPayload => {
-      this.setState({pets: petsPayload.content})
-    }) 
-  }
+const App = (props) => {
+  return (
+    <div>
+      <h2>Hello from React</h2>
+      <HomePageContainer/>
 
-  render() {
-    const petListItems = this.state.pets.map((pet) => {
-      return (<li><h2>{ pet.name }</h2><p>{ pet.species}</p></li>)
-    })
-    return (<ul>{petListItems}</ul>)
-  }
+    </div>
+  
+  );
 }
 
 ReactDom.render(<App />,document.getElementById("app"))
